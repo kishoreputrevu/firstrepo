5b9e4daecbfd8b7ebebb9a4be1980be451b4f2b6
unknown <kishore.putrevu@ideabytes.com>
2018-08-23 17:54:08 +0530

    kishore commit

---
+++ src/javaApplication1/JavaApplication2.java
@@ -0,0 +1,12 @@
+package javaApplication1;
+
+public class JavaApplication2 {
+
+    public static void main(String[] args) {
+    	int a=7,b=3,c;
+        c= a-b;
+        System.out.print(c);
+    }
+
+    	
+}
