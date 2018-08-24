ba51ff4125a1e2b3ccbc24b4a573ea0130e57e20
unknown <kishore.putrevu@ideabytes.com>
2018-08-24 14:07:06 +0530

    multiplication added

--- src/javaApplication1/JavaApplication2.java
+++ src/javaApplication1/JavaApplication2.java
@@ -6,7 +6,16 @@ public class JavaApplication2 {
     	int a=7,b=3,c;
         c= a-b;
         System.out.print(c);
+
+	int d = mul();
+	System.out.println(d);
+
     }
 
+    public int mul() {
+       int a = 10, b=2, c;
+ 	c = a*b;
+	return c;
+   }
     	
 }
