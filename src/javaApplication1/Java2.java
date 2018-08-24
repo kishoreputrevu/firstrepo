aa6b9d9a9cfdc5fc3616dded4d5def93f72bcbef
unknown <kishore.putrevu@ideabytes.com>
2018-08-24 14:20:47 +0530

    new file java2.java adding

---
+++ src/javaApplication1/Java2.java
@@ -0,0 +1,18 @@
+package javaApplication1;
+
+public class JavaApplication2 {
+
+	public int kishore(){
+		return 10;
+	}
+
+
+    public static void main(String[] args) {
+
+	int d = kishore();
+	System.out.println(d);
+
+    }
+
+    	
+}
